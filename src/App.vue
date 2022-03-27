50ac3b9b9696c5672caab2665faa0687e900d1f7
Vic <wangshi199030@gmail.com>
2022-03-27 14:30:11 +0800

    update Docs

--- src/App.vue
+++ src/App.vue
@@ -260,6 +260,29 @@
       :read_only="true"
       :languages="[['html', 'template']]"
     />
+    <a
+      href="https://github.com/justcaliturner/vue-resizer"
+      target="_blank"
+      class="link"
+      style="margin-top: 40px"
+    >
+      <span>View on Github</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-arrow-right"
+      >
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+        <polyline points="12 5 19 12 12 19"></polyline>
+      </svg>
+    </a>
   </div>
 </template>
 
@@ -635,7 +658,7 @@ props: {
 
 <style>
 body {
-  margin: 40px 0;
+  margin-top: 40px;
   font-family: sans-serif;
   background: #04111b;
 }
